221517845855befe0148e3cc3ddf66b5473fe636
MalayKedia <malay.kedia@gmail.com>
2024-04-15 23:13:44 +0530

    fixed checkout

--- submission.sh
+++ submission.sh
@@ -25,13 +25,13 @@ find_folder_by_hash() {
     fi
 }
 
-find_folder_my_message() {
+find_folder_by_message() {
     remote_repo="$1"
     message="$2"
-    count_of_matching_lines=$(grep -c "^[0-9]{16} : $message" $path_to_remote_repo/.git_log.txt)
+    count_of_matching_lines=$(grep -c -E "^[0-9]{16} : $message$" $path_to_remote_repo/.git_log.txt)
 
     if [ $count_of_matching_lines -eq 1 ]; then
-        hash=$(grep "^[0-9]{16} : $message" $path_to_remote_repo/.git_log.txt | cut -d ':' -f 1)
+        hash=$(grep -E "^[0-9]{16} : $message" $path_to_remote_repo/.git_log.txt | cut -d ':' -f 1)
         echo $(find_folder_by_hash $remote_repo $hash)
         return 0
     elif [ $count_of_matching_lines -gt 1 ]; then
@@ -39,6 +39,33 @@ find_folder_my_message() {
     else
         return 2
     fi
+}
+
+find_changed_files_between_commits() {
+    local hash1="$1"
+    local hash2="$2"
+    
+    local remote_repo=$(realpath $(readlink -f ./.my_git))
+    
+    local path1=$(find_folder_by_hash $remote_repo $hash1)
+    local path2=$(find_folder_by_hash $remote_repo $hash2)
+   
+    for file in $(ls $path1); do
+        if [ -f $path2/$file ]; then
+            difference=$(diff $path1/$file $path2/$file)
+            if [ -n "$difference" ]; then
+                echo "Changed: $file"
+            fi
+        else
+            echo "Removed: $file"
+        fi
+    done
+
+    for file in $(ls $path2); do
+        if [ ! -f $path1/$file ]; then
+            echo "Added: $file"
+        fi
+    done
 }
 
 # Exit if there is no command 0
@@ -236,7 +263,11 @@ if [ "$1" = 'git_commit' ]; then
             mkdir $path_to_remote_repo/$hash_value
             cp *.csv $path_to_remote_repo/$hash_value
             echo "All csv files committed to remote repository"
-            
+
+            second_last_hash=$(tail -n 2 $path_to_remote_repo/.git_log.txt | cut -d ':' -f 1)
+            if [ -n "$second_last_hash" ]; then
+                find_changed_files_between_commits $second_last_hash $hash_value      
+            fi
         fi
     else
         echo "Remote repository does not exist"
@@ -262,9 +293,12 @@ if [ "$1" = 'git_checkout' ]; then
     if [ -d "./.my_git" ]; then
         path_to_remote_repo=$(readlink -f ./.my_git)
         if [ -d $path_to_remote_repo ]; then
-            if [ "$2" = '-m' ]; then
+            if [ "$2" = 'HEAD' ]; then
+                latest_hash=$(tail -n 1 $path_to_remote_repo/.git_log.txt | cut -d ':' -f 1)
+                bash submission.sh git_checkout $latest_hash
+            elif [ "$2" = '-m' ]; then
                 message=$3
-                path_of_commit=$(find_folder_my_message $path_to_remote_repo $message)
+                path_of_commit=$(find_folder_by_message "$path_to_remote_repo" "$message")
                 return_code=$?
                 if [ $return_code -eq 0 ]; then
                     rm *.csv
@@ -303,8 +337,6 @@ if [ "$1" = 'git_checkout' ]; then
         exit 1
     fi
 fi
-
-
 
 if [ "$1" = 'help' ]; then
     echo "Usage: bash submission.sh <command> <any other extra arguments(if needed)>"
