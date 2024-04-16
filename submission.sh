e52e778edc216a0155b9c0fdbb847077ab0af673
MalayKedia <malay.kedia@gmail.com>
2024-04-16 16:54:21 +0530

    fixed combine

--- submission.sh
+++ submission.sh
@@ -1,5 +1,61 @@
 #!/bin/bash
 
+combine() {
+    touch main.csv
+    echo -n "Roll_Number,Name" > main.csv
+
+    # Check if all extra arguments are csv files
+    for file in "$@"; do
+        if [ -f "$file" ]; then
+            if [[ ! "$file" == *.csv ]]; then
+                echo "File $file is not a CSV file"
+                exit 1
+            fi
+        else
+            echo "File $file does not exist"
+            exit 1
+        fi
+    done
+    # Create the header
+    for file in "$@"; do
+        echo -n ",$(basename "$file" .csv )" >> main.csv
+    done
+    echo "" >> main.csv
+    # put the names and roll numbers
+    for file in "${@:2}"; do
+        awk -F, 'NR>1 {print $1","$2}' "$file" >> main.csv   
+    done >> main.csv
+    tail -n +2 main.csv | sort -k1 | uniq > temp.csv
+    header=$(head -n 1 main.csv)
+    echo $header > main.csv
+    cat temp.csv >> main.csv 
+    rm temp.csv
+
+    # put the marks
+    for file in "$@"; do
+        line_number_in_main=0
+        while read -r line || [ -n "$line" ]; do
+            ((line_number_in_main++))
+            if [ $line_number_in_main -eq 1 ]; then
+                continue  # Skip processing the header
+            fi
+            
+            roll_no="$(echo "$line" | cut -d ',' -f 1)"
+            
+            line_number_in_marks=$(cut -d ',' -f 1 "$file" | grep -n -m 1 "^$roll_no" | cut -d ':' -f 1)
+            
+            if [ -z "$line_number_in_marks" ]; then
+                sed -i "${line_number_in_main}s/$/,a/" main.csv
+            else
+                marks=$(head -n "$line_number_in_marks" "$file" | tail -n 1 | cut -d ',' -f 3)
+                sed -i "${line_number_in_main}s/$/,${marks}/" main.csv
+            fi
+            
+        done < main.csv
+    done >> main.csv    
+
+}
+
 generate_hash() {
     local random_number=""
 
@@ -84,74 +140,21 @@ fi
 if [ "$1" = 'combine' ]; then
     total_run_before=false
     if [ -f "main.csv" ]; then
-        total_exists=$(head -n 1 main.csv | grep "total")
+        total_exists=$(head -n 1 main.csv | awk -F',' '{print $NF}' | grep "^total$")
         if [ -n "$total_exists" ]; then
             total_run_before=true
         fi
     fi
-    touch main.csv
-    echo -n "Roll_Number,Name" > main.csv
-    if [ $# -gt 2 ]; then
-        # Check if all extra arguments are csv files
-        for file in "${@:2}"; do
-            if [ -f "$file" ]; then
-                if [[ ! "$file" == *.csv ]]; then
-                    echo "File $file is not a CSV file"
-                    exit 1
-                fi
-            else
-                echo "File $file does not exist"
-                exit 1
-            fi
-        done
-        # Create the header
-        for file in "${@:2}"; do
-            echo -n ",$(basename "$file" .csv )" >> main.csv
-        done
-        echo "" >> main.csv
-        # put the names and roll numbers
-        for file in "${@:2}"; do
-            awk -F, 'NR>1 {print $1","$2}' "$file" >> main.csv   
-        done >> main.csv
-        tail -n +2 main.csv | sort -k1 | uniq > temp.csv
-        header=$(head -n 1 main.csv)
-        echo $header > main.csv
-        cat temp.csv >> main.csv 
-        rm temp.csv
-
-        # put the marks
-        for file in "${@:2}"; do
-            line_number_in_main=0
-            while read -r line || [ -n "$line" ]; do
-                ((line_number_in_main++))
-                if [ $line_number_in_main -eq 1 ]; then
-                    continue  # Skip processing the header
-                fi
-               
-                roll_no="$(echo "$line" | cut -d ',' -f 1)"
-               
-                line_number_in_marks=$(cut -d ',' -f 1 "$file" | grep -n -m 1 "^$roll_no" | cut -d ':' -f 1)
-               
-                if [ -z "$line_number_in_marks" ]; then
-                    sed -i "${line_number_in_main}s/$/,a/" main.csv
-                else
-                    marks=$(head -n "$line_number_in_marks" "$file" | tail -n 1 | cut -d ',' -f 3)
-                    sed -i "${line_number_in_main}s/$/,${marks}/" main.csv
-                fi
-               
-            done < main.csv
-        done >> main.csv
-
-        if [ "$total_run_before" = true ]; then
-            bash submission.sh total
-        fi
-    elif [ $# -eq 2 ]; then
-        # If only one extra argument is provided, assume it to be a directory and combine all csv files in the directory
-        filenames=$(ls $2/*.csv)
-        bash submission.sh combine $filenames
-    elif [ $# -eq 1 ]; then
+    if [ $# -gt 1 ]; then
+        combine ${@:2}
+    else
         # If no extra arguments are provided, combine all csv files in the directory
-        bash submission.sh combine .
+        files=$(ls *.csv | grep -v "main.csv")
+        combine $files
+    fi
+
+    if [ "$total_run_before" = true ]; then
+        bash submission.sh total
     fi
 fi
 
